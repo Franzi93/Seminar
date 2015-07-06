63e86d6286bea4ae67b580935268b7f4ba8be017
Tohuwabohu-1984 <HeikoQ@gmail.com>
2015-07-06 20:33:06 +0200

    Erster Wegpunkt auf der Erde(es leuchtet gruengit add --all)

---
+++ space/Assets/Skript/Waypoint.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waypoint : MonoBehaviour {
+
+	public bool _passiert = false;
+
+	void Start() {
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		_passiert = true;
+		Light li = GetComponent<Light>();
+		if(li != null)	{li.color = Color.green;}
+		//Destroy(other.gameObject);
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+	
+		if (_passiert) {
+			Light li = GameObject.FindGameObjectWithTag("WaypointLight").GetComponent<Light>();
+			if(li != null){li.color = Color.green;}
+		}
+	}
+}
