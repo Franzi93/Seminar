426f6a88bc28c0d8ad1f84233f2fa64238c292c7
Tohuwabohu-1984 <HeikoQ@gmail.com>
2015-07-10 15:54:27 +0200

    Letzte Anpassungen kurz vor ele-pitch. Timer + Verb. HUD

--- space/Assets/Skript/Kamera.cs
+++ space/Assets/Skript/Kamera.cs
@@ -21,8 +21,8 @@ public class Kamera : MonoBehaviour {
 		}
 
 	}
-
 	
+
 	void LateUpdate(){
 
 		GameObject go_FirstPersCam = GameObject.FindGameObjectWithTag("FirstPersCam");
--- space/Assets/Skript/WayPointControl.cs
+++ space/Assets/Skript/WayPointControl.cs
@@ -13,6 +13,12 @@ public class WayPointControl : MonoBehaviour {
 		_alleWegpunktePassiert = false;
 		_waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
 	}
+
+	void OnGUI() {
+		
+		float _ZeitSecSpielstart = Time.realtimeSinceStartup;
+		string ToEdit = GUI.TextField(new Rect(10, 10, 100, 20),"Time: " + _ZeitSecSpielstart.ToString(), 25);
+	}
 	
 	// Update is called once per frame
 	void Update () 
@@ -20,37 +26,42 @@ public class WayPointControl : MonoBehaviour {
 		//Debug.Log("TEST");
 		//Debug.LogFormat("Time.deltaTime:  {0} ", Time.deltaTime);
 
-
 		int naechsterWegpunktIndex = _zuletztMarkierterWegpunkt_index + 1;
 
 		foreach (GameObject go_wp in _waypoints)
 		{
 			Waypoint wp = go_wp.GetComponent<Waypoint>();
 			wp._istDerResetWp = false;
+
 			if(naechsterWegpunktIndex == go_wp.GetComponent<Waypoint>()._nummer)
 			{
-				wp._istDerNaechsteWp = true;
+				wp._istDerNaechsteWp = true; // Festlegen des nächsten Wegpunkts
 			}
 
 			if(_zuletztMarkierterWegpunkt_index == go_wp.GetComponent<Waypoint>()._nummer)
 			{
-				wp._istDerResetWp = true;
+				wp._istDerResetWp = true; // Festlegen des Punktes auf den der Spieler bei Tot zuück gesetzt wird.
 			}
 
 			if(wp._istZiel && wp._istPassiert)
 			{
-				_alleWegpunktePassiert = true;
+				_alleWegpunktePassiert = true; // Flag ob alle Wegpunkte durchlaufen wurden ( implizites Ende des Spiels)
 			}	
 		} // .. foreach (GameObject go_wp in _waypoints)
 
+		//float secSpielstart = Time.realtimeSinceStartup;
+		//Debug.Log("SEKUNDEN: " + secSpielstart.ToString());
 
+
+		// Spielende
 		if(_alleWegpunktePassiert)
 		{
+			//float secSpielende = Time.unscaledTime;
+			//Debug.Log("SEKUNDEN_secSpiel: " + secSpielende.ToString());
 
-			//if(Time.deltaTime > 4f)
+			//if(secSpielende * 100 > 4f)
 			{
-
-				Application.LoadLevel(0);
+				Application.LoadLevel(0);// Laden des Level-Select-Menüs
 			}
 		}
 	}// .. void Update () 
